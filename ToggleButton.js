6c7b71e7e1804daa41a49807ef167019cbec9a02
shashankgupta124 <shashankgupta124@gmail.com>
2025-04-28 17:38:25 +0530

    Add heading

--- ToggleButton.js
+++ ToggleButton.js
@@ -5,8 +5,11 @@ export default function ToggleButton(){
     const [isOn, setIsOn] = useState(false);
 
     return(
+        <>
+        <h2>Toggle Button</h2>
         <button onClick={() => setIsOn(i => !i)} >{ 
             isOn ? 'ON' : 'OFF'
         }</button>
+        </>
     )
  }
